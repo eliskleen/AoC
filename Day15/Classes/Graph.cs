388cd5a67dacd94a3de730b932434809285507c8
Elis Kleen <elisk@student.chalmers.se>
2021-12-17 06:17:57 +0100

    small refactor

--- Day15/Classes/Graph.cs
+++ Day15/Classes/Graph.cs
@@ -11,9 +11,10 @@ namespace Day15.Classes
         public Coord[,] grid { get; set; } 
         public Graph(List<string> lines, bool replicate = false)
         {
+            var repSize = 20;
             grid = new Coord[lines[0].Count(), lines.Count()];
             int x = 0, y = 0;
-            var newGrid = new Coord[lines[0].Count() * 5, lines.Count()*5];
+            var newGrid = new Coord[lines[0].Count() * repSize, lines.Count()*repSize];
             foreach (var line in lines)
             {
                 x = 0;
@@ -28,9 +29,9 @@ namespace Day15.Classes
             if(!replicate)
                 return;
             
-            for(int repy = 0; repy < 5; repy++)
+            for(int repy = 0; repy < repSize; repy++)
             {
-                for(int repx = 0; repx < 5; repx++)
+                for(int repx = 0; repx < repSize; repx++)
                 {
                     if(repx == 0 && repy == 0)
                         continue;
@@ -42,12 +43,9 @@ namespace Day15.Classes
                             y = j + (repy*grid.GetLength(1));
                             var getx = repx > 0 ? x-grid.GetLength(0) : x;
                             var gety = repx == 0 ? y-grid.GetLength(1) : y;
-                            //y = y > 9 ? y-10 : y;
-                            //Console.WriteLine(x + "," + y + "," + repx);
                             var newRisk = (newGrid[getx, gety].risk + 1);
                             var c = new Coord(x, y, newRisk > 9 ? 1 : newRisk);
                             newGrid[x, y] = c;
-                            //Console.WriteLine(x);
                         }
                     }
                 }
