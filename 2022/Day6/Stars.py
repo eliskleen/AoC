d5eef6d0af7544606c89ebc483a6720ba6dc59f8
Elis Kleen <eliskleen00@gmail.com>
2022-12-06 06:15:09 +0100

    das quick lol

---
+++ 2022/Day6/Stars.py
@@ -0,0 +1,62 @@
+import time
+
+def day_():
+    path = "data.txt"
+    # path = "test-data.txt"
+
+    start_time = time.perf_counter()
+    data = get_data(path)
+
+    time1 = time.perf_counter()
+
+    ans1 = star1(data)
+    time2 = time.perf_counter()
+
+    ans2 = star2(data)
+    time3 = time.perf_counter()
+
+    load_time = time1 - start_time
+    star1_time = time2 - time1
+    star2_time = time3 - time2
+    if 1:
+        print(f'Load time: {load_time}')
+        print(f'Star 1 time: {star1_time}')
+        print(f'Star 2 time: {star2_time}')
+        print(f'Star 1 answer: {ans1}')
+        print(f'Star 2 answer: {ans2}')
+
+def get_data(path):
+    with open(path) as f:
+        return f.read()
+    
+def star1(data):
+    for i in range(len(data)-4):
+        s = data[i:i+4]
+        seen = set()
+        dupes = len([x for x in s if x in seen or seen.add(x)])
+        if dupes == 0:
+            return i+4
+def star2(data):
+    for i in range(len(data)-14):
+        s = data[i:i+14]
+        seen = set()
+        dupes = len([x for x in s if x in seen or seen.add(x)])
+        if dupes == 0:
+            return i+14
+
+
+    return 0
+
+def main():
+    import cProfile
+    import pstats
+    with cProfile.Profile() as pr:
+        day_()
+    
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    # stats.print_stats()
+    stats.dump_stats(filename = 'profiling.prof')
+
+if __name__ == '__main__':
+    main()
