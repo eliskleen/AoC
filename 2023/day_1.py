4a6899da0b3bbcda18a379d87ca735a58fd66d99
eliskleen <eliskleen00@gmail.com>
2023-12-01 06:40:22 +0100

    day 1 done

---
+++ 2023/day_1.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import time
+from aocd import submit
+from aocd.models import Puzzle
+
+
+def day_():
+    year = int(os.getcwd().split('\\')[-1][:4]) 
+    day = int(__file__.split('\\')[-1].split('_')[1].split('.')[0])
+    puzzle = Puzzle(year=year, day=day) 
+    submit_a = "a" in sys.argv
+    submit_b = "b" in sys.argv
+    example = "e" in sys.argv
+
+    if (submit_a or submit_b) and example:
+        print("Cannot submit examples")
+        return
+    
+    raw_data = ""
+    if example:
+        print("Using example")
+        #use 'aocd year day --example' to get the example data
+        with open('example.txt', 'r') as f:
+            raw_data = f.read()
+    else:
+        raw_data = puzzle.input_data
+            
+    start_time = time.perf_counter()
+    data = format_data(raw_data)
+
+    time1 = time.perf_counter()
+
+    ans1 = star1(data)
+    time2 = time.perf_counter()
+
+    ans2 = star2(data)
+    time3 = time.perf_counter()
+
+    load_time = time1 - start_time
+    star1_time = time2 - time1
+    star2_time = time3 - time2
+
+    if submit_a:
+        print("Submitting star 1")
+        puzzle.answer_a = ans1
+    if submit_b:
+        print("Submitting star 2")
+        puzzle.answer_b = ans2
+    if 1:
+        print(f'Load time: {load_time}')
+        print(f'Star 1 time: {star1_time}')
+        print(f'Star 2 time: {star2_time}')
+        print(f'Star 1 answer: {ans1}')
+        print(f'Star 2 answer: {ans2}')
+
+
+def format_data(raw):
+
+    return raw.split('\n')
+    
+def star1(data):
+    ret = 0
+    for line in data:
+        nums = [int(x) for x in line if x.isdigit()]
+        ret += nums[0]*10 + nums[-1]
+    return ret
+
+def star2(data):
+    ret = 0
+    for line in data:
+        i = 0
+        nums = []
+        while i < len(line):
+            for word in fromWord:
+                if line[i:].startswith(word):
+                    nums.append(fromWord[word])
+                    break
+            i += 1
+        ret += nums[0]*10 + nums[-1]
+    return ret
+
+fromWord = {
+    '1' : 1,
+    '2' : 2,
+    '3' : 3,
+    '4' : 4,
+    '5' : 5,
+    '6' : 6,
+    '7' : 7,
+    '8' : 8,
+    '9' : 9,
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four' : 4,
+    'five' : 5,
+    'six' : 6,
+    'seven' : 7,
+    'eight' : 8,
+    'nine' : 9
+}
+
+def main():
+    import cProfile
+    import pstats
+    with cProfile.Profile() as pr:
+        day_()
+    
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    # stats.print_stats()
+    day = int(__file__.split('\\')[-1].split('_')[1].split('.')[0]) 
+    stats.dump_stats(filename = f'profiling\\profiling{day}.prof')
+
+# run with `py day_n.py -- a b` to submit both stars for day n
+if __name__ == '__main__':
+    main()
